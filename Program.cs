04589b3c8ab3caa68ae346743d288f2ffde844ed
Lars Bendik Bjella Ramstad <brlb022025@gmail.com>
2025-03-18 14:48:21 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Format_alphanumeric_data_for_presentation;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string first = "Nei";
+        string second = "takk";
+        Console.WriteLine("{0} {1}!", first, second);
+        Console.WriteLine("{0} {0} {0}!", first, second);
+
+        Console.WriteLine($"{first} {second}!");
+        Console.WriteLine($"{second} {first}!");
+        Console.WriteLine($"{first} {first} {first}!");
+    }
+
+}
