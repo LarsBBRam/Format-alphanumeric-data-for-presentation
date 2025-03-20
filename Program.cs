2c5e96cc21a0803daa63efe5076a6c653fec1f49
Lars Bendik Bjella Ramstad <brlb022025@gmail.com>
2025-03-20 09:59:43 +0100

    final commit

--- Program.cs
+++ Program.cs
@@ -1,17 +1,82 @@
-﻿namespace Format_alphanumeric_data_for_presentation;
+﻿using System.Runtime.InteropServices;
+
+namespace Format_alphanumeric_data_for_presentation;
 
 class Program
 {
     static void Main(string[] args)
     {
+        /*
         string first = "Nei";
         string second = "takk";
+
+        //composite formatting
         Console.WriteLine("{0} {1}!", first, second);
         Console.WriteLine("{0} {0} {0}!", first, second);
 
+        //string interpolation
         Console.WriteLine($"{first} {second}!");
         Console.WriteLine($"{second} {first}!");
         Console.WriteLine($"{first} {first} {first}!");
+
+        //formatting currency
+        decimal price = 123.45m;
+        int discount = 50;
+        Console.WriteLine($"Price: {price:C} (Save {discount:C})");
+       
+        //formatting numbers
+        decimal measurement = 123456.78912m;
+        Console.WriteLine($"Measurement: {measurement:N4} units");
+
+        //formatting percentages
+        decimal tax = .36785m;
+        Console.WriteLine($"Tax rate: {tax:P2}");
+
+        //Combining formatting approaches
+        decimal priceIs = 67.55m;
+        decimal salePrice = 59.99m;
+
+        string yourDiscount = string.Format("You saved {0:C2} off the regular {1:C2} price. ", (priceIs - salePrice), priceIs);
+
+        yourDiscount += $"A discount of {((priceIs - salePrice) / priceIs):P2}!"; //inserted
+        Console.WriteLine(yourDiscount);
+
+        int invoiceNumber = 1201;
+        decimal productShares = 25.4568m;
+        decimal subtotal = 2750.00m;
+        decimal taxPercentage = .15825m;
+        decimal total = 3185.19m;
+
+        Console.WriteLine($"Invoice Number: {invoiceNumber}");
+        Console.WriteLine($"    Shares: {productShares:N3} Product");
+        Console.WriteLine($"      Sub Total: {subtotal:C} ");
+        Console.WriteLine($"        Tax: {taxPercentage:P2}");
+        Console.WriteLine($"      Total Billed: {total:C}");
+
+        string input = "Pad this";
+        Console.WriteLine(input.PadLeft(12, '-'));
+        Console.WriteLine(input.PadRight(12, '-'));
+
+
+        //working with padded strings
+
+
+        string paymentId = "769C";
+        string payeeName = "Mr. Stephen Ortega";
+        string paymentAmount = string.Format($"{50000:C}");
+
+        var formattedLine = paymentId.PadRight(6);
+        formattedLine += payeeName.PadRight(22);
+        formattedLine += paymentAmount.PadLeft(8);
+
+        Console.WriteLine("1234567890123456789012345678901234567890");
+        Console.WriteLine("||||||||||||||||||||||||||||||||||||||||");
+        Console.WriteLine(formattedLine);
+        */
+
+
+
+
     }
 
 }
